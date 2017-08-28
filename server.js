10f821df5378771a17266bbf856b5c33704c82a4
Romain <romain.nicaise@fleet-performance.com>
2017-08-28 14:15:53 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+var mqtt = require('mqtt')
+var moment = require('moment');
+
+var client  = mqtt.connect({ host: 'localhost', port: 1883 })
+ 
+ client.on('connect', function () {
+  client.subscribe("alpha2/test")
+})
+ 
+client.on('message', function (topic, message) {
+  // message is Buffer 
+  console.log(message.toString())
+  client.end()
+})
